2b6909fed7f6a2c7ad3968f95627245d3c17c241
spiff-enough <aury.casanova@gmail.com>
2015-04-05 18:47:20 -0600

    Used resumeBuilder.js to append my name and role to the header of index.html

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,14 @@
-$("#main").append("Aury Casanova");
+/*$("#main").append("Aury Casanova");
+
+var awesomeThoughts = "I am Aury and I am AWESOME!";
+console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+$("#main").append(funThoughts);*/
+var name = "Aury Casanova";
+var role = "Software Developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
